a13964b3aad44d12b9c4aff025ff64ed99254e4c
D-G-D <157231290+D-G-D@users.noreply.github.com>
2024-02-14 18:31:35 +0100

    Update py-06.py

--- Code/py-06.py
+++ Code/py-06.py
@@ -5,8 +5,15 @@
 %cd py_course
 %pwd
 
+# Alternative using Python Functions
+import os
+os.getcwd()
+os.chdir('C:\\Users\\dirkg\\OneDrive\\Professional\\Python')
+os.chdir(r'C:\Users\dirkg\OneDrive\Professional\Python')
+
 # Program 1 #
 print('\nHi, there!\nThis is the Python Course!')
+
 runfile('program1.py')
 
 # input statements #
@@ -14,10 +21,28 @@ input('\nDo you know Python? (y/n) ')
 int(input('\nWhat is your age? '))
 
 # Program 2 #
+answer = input('\nDo you know Python? (y/n) ')
+
+if answer == 'y':
+    print('\nCool!!')
+else:
+    print('\nSorry about that.')
+    
 runfile('program2.py')
 
 # Modules #
-import myfuncs
-myfuncs.has_zero(1, -2, 0)
-from myfuncs import has_zero
-has_zero(1, -2, 3)
+month_list = ['January', 'February', 'March', 'April', 'May', 'June',
+    'July', 'August', 'September', 'October', 'November', 'December']
+
+def convert(date):
+    year = date[:4]
+    month_no = int(date[5:7])
+    month = month_list[month_no - 1]
+    day = str(int(date[8:]))
+    return month + ' ' + day + ', ' + year
+
+import datestuff
+datestuff.convert('1954-04-30')
+
+from datestuff import convert
+convert('1954-04-30')
